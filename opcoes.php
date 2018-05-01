9adeb60e82933e102defcc6c3a22a57909c3bd90
Edevaldo Mazzucco <edevaldomazzucco1999@gmail.com>
2018-05-01 13:50:45 -0300

    Adicionado a pagina opcoes com avatares dos operadores

--- opcoes.php
+++ opcoes.php
@@ -35,7 +35,76 @@ if($autorizado == true){
 
 <?php require_once("visual/header.php") ?>
 
-Página de Opções
+<div class="centro">
+	<div class="bloco-usuarios">
+		<div class="bloco">
+			<div class="imagem">
+				<img src="img/avatar-user.png">
+			</div>
+			<div class="informacoes-usuario">
+				<h2>Edevaldo Mazzucco</h2>
+				<small>Administrador Geral</small>
+				<a href="" class="btn-user-info"><i class="far fa-question-circle"></i> Informações</a>
+				<a href="" class="btn-user-edit"><i class="fas fa-pencil-alt"></i> Editar</a>
+			</div>
+		</div>
+		<div class="bloco">
+			<div class="imagem">
+				<img src="img/avatar-user.png">
+			</div>
+			<div class="informacoes-usuario">
+				<h2>Marcio Santos</h2>
+				<small>Administrador Geral</small>
+				<a href="" class="btn-user-info"><i class="far fa-question-circle"></i> Informações</a>
+				<a href="" class="btn-user-edit"><i class="fas fa-pencil-alt"></i> Editar</a>
+			</div>
+		</div>
+		<div class="bloco">
+			<div class="imagem">
+				<img src="img/avatar-user.png">
+			</div>
+			<div class="informacoes-usuario">
+				<h2>Alessandra Fernandes</h2>
+				<small>Operador I</small>
+				<a href="" class="btn-user-info"><i class="far fa-question-circle"></i> Informações</a>
+				<a href="" class="btn-user-edit"><i class="fas fa-pencil-alt"></i> Editar</a>
+			</div>
+		</div>
+		<div class="bloco">
+			<div class="imagem">
+				<img src="img/avatar-user.png">
+			</div>
+			<div class="informacoes-usuario">
+				<h2>Paulo Henrique Santos</h2>
+				<small>Operador I</small>
+				<a href="" class="btn-user-info"><i class="far fa-question-circle"></i> Informações</a>
+				<a href="" class="btn-user-edit"><i class="fas fa-pencil-alt"></i> Editar</a>
+			</div>
+		</div>
+		<div class="bloco">
+			<div class="imagem">
+				<img src="img/avatar-user.png">
+			</div>
+			<div class="informacoes-usuario">
+				<h2>Alexandro Oliveira</h2>
+				<small>Operador I</small>
+				<a href="" class="btn-user-info"><i class="far fa-question-circle"></i> Informações</a>
+				<a href="" class="btn-user-edit"><i class="fas fa-pencil-alt"></i> Editar</a>
+			</div>
+		</div>
+		<div class="bloco">
+			<div class="imagem">
+				<img src="img/avatar-user.png">
+			</div>
+			<div class="informacoes-usuario">
+				<h2>Ana Julia Herzen</h2>
+				<small>Estágiario(a)</small>
+				<a href="" class="btn-user-info"><i class="far fa-question-circle"></i> Informações</a>
+				<a href="" class="btn-user-edit"><i class="fas fa-pencil-alt"></i> Editar</a>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php  require_once("visual/rodape.php") ?>
 
